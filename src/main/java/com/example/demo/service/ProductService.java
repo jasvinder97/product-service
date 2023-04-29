780fc130972a8e0b55d4140b6d4931dcaa364811
Jasvinder Sharma <sharmajasvinder9@gmail.com>
2023-04-29 22:22:04 +0530

    Product MicroService - Improvements

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,54 +1,22 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Product;
-import com.example.demo.repository.ProductRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@Slf4j
-public class ProductService {
+public interface ProductService {
+     Product saveProduct(Product product);
 
-    @Autowired
-    ProductRepository productRepository;
+     List<Product> saveMultipleProduct(List<Product> product);
 
-    public Product saveProduct(Product product) {
-        log.info("Save Product Invoked");
-        return productRepository.saveAndFlush(product);
-    }
+     List<Product> getAllProducts();
 
-    public List<Product> saveMultipleProduct(List<Product> product) {
-        log.info("Save Multiple Product Invoked");
-        return productRepository.saveAllAndFlush(product);
-    }
+    Product findProduct(Integer id);
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
-    }
+    Product findProductByName(String productName);
 
-    public Product findProduct(Integer id) {
-        log.info("Save Product Invoked");
-        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product Not Found"));
-    }
+    String deleteById(Integer id);
 
-    public Product findProductByName(String productName) {
-        log.info("Save Product Invoked");
-        return productRepository.findByName(productName).orElseThrow(() -> new RuntimeException("Product Not Found"));
-    }
+    Product updateProduct(Product product);
 
-    public String deleteById(Integer id) {
-        productRepository.deleteById(id);
-        return "Deleted Successfully";
-    }
-
-    public Product updateProduct(Product product) {
-        Product existingProduct = productRepository.findById(product.getId()).orElseThrow(() -> new RuntimeException("Product Not Found"));
-        existingProduct.setPrice(product.getPrice());
-        existingProduct.setName(product.getName());
-        existingProduct.setQuantity(product.getQuantity());
-        return productRepository.saveAndFlush(existingProduct);
-    }
 }
