a5257f3892d9217ac020a74fddbd6b937d8d1ea8
Jasvinder Sharma <sharmajasvinder9@gmail.com>
2023-04-30 20:30:24 +0530

    Product MicroService - @Column in Product

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -6,7 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -20,7 +24,18 @@ public class Product {
     @Id
     @GeneratedValue
     private Integer id;
+    @Column(name = "product_name")
     private String name;
+    @Column(name = "product_quantity")
     private Integer quantity;
+    @Column(name = "product_price")
     private Double price;
+
+    @CreatedDate
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @LastModifiedDate
+    @Column(name = "modified_on")
+    private LocalDateTime modifiedBy;
 }
